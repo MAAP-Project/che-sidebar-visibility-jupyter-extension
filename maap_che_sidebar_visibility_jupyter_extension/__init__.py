080f9c3c27cc5ebaa2e2d8c382cdab124b63deca
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2024-03-18 11:47:00 -0700

    changed ext name | bumped version

--- maap_che_sidebar_visibility_jupyter_extension/__init__.py
+++ maap_che_sidebar_visibility_jupyter_extension/__init__.py
@@ -4,6 +4,6 @@ from ._version import __version__
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "che-sidebar-visibility-jupyter-extension"
+        "dest": "maap-che-sidebar-visibility-jupyter-extension"
     }]
 
